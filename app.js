1696f9921f8e3b9c52336c3db8701dda03246818
THUMMALAPALLI NIKHIL CHOWDARY <tnikhilchowdary@gmail.com>
2025-07-29 19:50:24 -0500

    Initial commit

---
+++ app.js
@@ -0,0 +1,11 @@
+//Recalling all Nodejs Topics for interview
+const amount = 9
+
+if(amount < 10){
+    console.log("Small Number");
+}
+else{
+    console.log("Large Number");
+}
+
+console.log("Hey its my first Node App.")
